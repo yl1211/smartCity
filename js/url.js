04aa877dd7e570d3f6d2c180cc32fe35b08faa68
yl1211 <344409304@qq.com>
2019-03-22 17:40:06 +0800

    卡包2

--- js/url.js
+++ js/url.js
@@ -1,3 +1,4 @@
-var globalurl = "http://192.168.1.118:8080/gateway";     //局域网地址
+// var globalurl = "http://192.168.1.118:8080/gateway";     //王艳良局域网地址
+var globalurl = "http://192.168.1.130:8080/gateway";     //王艳良局域网地址
 // var globalurl = "http://1.192.121.127:12613/gateway";     //映射地址
 
