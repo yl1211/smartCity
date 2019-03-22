3c00571476fe5e0285fe93ebfa5d98fb7bec06e0
yl1211 <344409304@qq.com>
2019-03-22 14:44:03 +0800

    卡包

--- js/url.js
+++ js/url.js
@@ -1,3 +1,3 @@
-// var globalurl = "http://192.168.1.118:8080/gateway";     //局域网地址
-var globalurl = "http://1.192.121.127:12613/gateway";     //映射地址
+var globalurl = "http://192.168.1.118:8080/gateway";     //局域网地址
+// var globalurl = "http://1.192.121.127:12613/gateway";     //映射地址
 
