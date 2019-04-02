01de7432d9787350dd6071a1982c7044d884040a
yl1211 <344409304@qq.com>
2019-04-02 08:41:33 +0800

    xuexiao

--- js/url.js
+++ js/url.js
@@ -1,5 +1,5 @@
-// var globalurl = "http://192.168.1.118:8080/gateway";     //王艳良局域网地址
+var globalurl = "http://192.168.1.118:8080/gateway";     //王艳良局域网地址
 // var globalurl = "http://192.168.1.130:8080/gateway";     //孙宇飞局域网地址
-var globalurl = "http://192.168.1.114:8085/gateway";     //刘其松8085局域网地址
+// var globalurl = "http://192.168.1.114:8085/gateway";     //刘其松8085局域网地址
 // var globalurl = "http://1.192.121.127:12613/gateway";     //映射地址
 
