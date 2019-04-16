399446db41bdaa4b157a506d0456ba7dc8eebab4
yl1211 <344409304@qq.com>
2019-04-16 08:30:14 +0800

    积分兑换

--- js/url.js
+++ js/url.js
@@ -1,4 +1,4 @@
-// var globalurl = "http://192.168.1.121:8080/gateway";     //王艳良局域网地址
+// var globalurl = "http://192.168.1.118:8080/gateway";     //王艳良局域网地址
 var globalurl = "http://192.168.1.112:8080/gateway";     //孙宇飞局域网地址
 // var globalurl = "http://192.168.1.111:8080/gateway";     //刘其松局域网地址
 // var globalurl = "http://192.168.1.111:8085/gateway";     //刘其松8085局域网地址
